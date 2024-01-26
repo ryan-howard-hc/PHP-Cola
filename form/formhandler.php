9952fdbd7685e4c1512cfbaf95e484dcba857a36
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2024-01-26 11:09:17 -0500

    messing with a form for practice

---
+++ form/formhandler.php
@@ -0,0 +1,18 @@
+<?php
+
+var_dump($_SERVER["REQUEST_METHOD"]);
+//
+
+// if($_SERVER["REQUEST_METHOD"]) {  
+
+
+// }
+
+?>
+
+
+<!-- get, ofc, typically for showing something to show user etc etc -->
+<!-- post typically for submit data -->
+
+
+<!-- form folder ofc, in practice, supposed to be a private directory in server -->
---
+++ formindex.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<main>
+    <form action ="includes/formhandler.php" method = "post">
+    <!-- <form action ="<?php echo $_SERVER["PHP_SELF"];?>" method = "post"> -->
+        <!-- submit to same page youre inside of. NOT RECOMMENDED, XSS and hacking vulnerable -->
+    
+        <label for = "firstName"> First name: </label>
+        <input id ="firstName" type='text' name = 'firstName' placeholder="...">
+
+        <label for = "lastName"> Surname: </label>
+        <input id ="lastName" type='text' name = 'lastName' placeholder="...">
+
+        <label for= 'favoriteGame'>Favorite Game?</label>
+        <select id = 'favoriteGame' name = 'favoriteGame'>
+            <option value="none">None</option>
+            <option value="forza">Forza Motorsports</option>
+            <option value="callOfDuty">Call of Duty</option>
+            <option value="fallout">Fallout Series</option>
+
+        </select>
+
+        <button type = "submit">Submit</button>
+    </form>
+
+</main>
+
+
+</body>
+</html>
